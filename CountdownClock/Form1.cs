581abaf901494d79c4e9d9c496ea7f9f6824534b
Cuong Duy Nguyen <103cuong@gmail.com>
2017-04-05 21:58:07 +0700

    Add intial source

---
+++ CountdownClock/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountdownClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dtpk.Value = DateTime.Now;
+            
+        }
+
+        private void bunifuDatepicker1_onValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuMaterialTextbox1_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dtpk.Value <= DateTime.Now)
+            MessageBox.Show("Please select a reasonable time", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                if(bunifuCheckbox1.Checked)
+                {
+                    string[] text =new string[] {tbTitle.Text,dtpk.Value.ToString() };
+                    File.Instance.writeFile(text);
+                    
+                }
+                else
+                {
+                    string[] text = new string[] {"",""};
+                    File.Instance.writeFile(text);
+                }
+                Form2 f = new Form2(tbTitle.Text, dtpk.Value);
+                this.Hide();
+                f.ShowDialog();
+                this.Show();
+            }
+           
+         
+         
+      
+        }
+
+      
+
+        private void dtpk_onValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] text=File.Instance.readFile();
+            if (text[0] !="")
+                tbTitle.Text = text[0];
+            
+            if (text[1] !="")
+                dtpk.Value = Convert.ToDateTime(text[1]);
+           
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+# Countdown
+
+⏳ A countdown app using C#.
+
+<p align="center">
+<img src="https://user-images.githubusercontent.com/34389409/49017001-47771e00-f1ba-11e8-8d6f-9c597ec0db36.gif" width="600"/>
+</p>
+
+## Contents
+
+* [Installation](#installation)
+* [Demo](#demo)
+* [Documents](#documents)
+* [Bugs and Issues](#bugs-and-issues)
+* [Authors](#authors)
+* [License](#license)
+
+## Installation
+
+**1. Create a C# Windows Forms Application**
+
+* Create a C# Windows Forms Project.
+* Add New Item to your application.
+
+**2. Create a Setup Project**
+
+* Add new project => setup and deployment => setup project.
+* Right Click on Setup project and Add project Output and select primary output from your main project.
+* Right Click on Setup project and Add project Output and select content files from your main project.
+* Select .Net Framework.
+* Select Windows Installer.
+* Select radio button Download prerequisites from the same location as my application.
+* Right Click on Users Desktop at left pane and add new Shortcut and select application folder, primary output from SampleApplication, and click ok and the rename the short cut to what you need.
+* Rebuild solution.
+* Rebuild Setup Project.
+* Go to Output directory of setup project and run setup.exe.
+
+Enjoy 😍
+
+## Demo
+
+You can use this app [here](https://drive.google.com/file/d/18KXJYTCx-77Fm9jR-nFuBNmlnb07WDdL/view?usp=sharing).
+
+## Documents
+
+For help getting started with C#, view our online [documentation](https://docs.microsoft.com/en-us/dotnet/csharp/).
+
+## License
+
+MIT
